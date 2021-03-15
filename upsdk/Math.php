dfc883432ec7230854aab885068ae574e7c53b61
cfn <cfn@leapy.cn>
2021-03-15 13:58:05 +0800

    init

---
+++ upsdk/Math.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * User: cfn
+ * Date: 2021/3/15
+ * Email: <cfn@leapy.cn>
+ */
+
+/**
+ * 数学函数
+ * Class Math
+ */
+class Math
+{
+    /**
+     * 加法
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
